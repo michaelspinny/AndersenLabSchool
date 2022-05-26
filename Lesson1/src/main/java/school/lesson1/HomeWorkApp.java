bdbd07049647a4c4d476a4f1afc3ff5dc7b1d01a
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-05-26 22:47:33 +0300

    Updated printColor() method

--- Lesson1/src/main/java/school/lesson1/HomeWorkApp.java
+++ Lesson1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -31,7 +31,7 @@ public class HomeWorkApp {
             System.out.println("Красный");
         } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
-        } else if (value > 100) {
+        } else {
             System.out.println("Зеленый");
         }
     }
